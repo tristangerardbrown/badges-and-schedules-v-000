6a0157f8949cda314276b210b7f998fb46b9238d
Tristan Brown <tgbrown@stanford.edu>
2018-08-20 18:59:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,11 +2,8 @@ def badge_maker(name)
 "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
-  counter = 0
-  "Hello, my name is #{array[0]}."
-  while counter <=6
-  counter += 1
+def batch_badge_creator(names)
+  array = []
+  names.each {|name| array.push("badge_marker(name)")}
+  array
 end
-end 
