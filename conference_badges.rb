caae98f071cff173a51bc395a12aa77c7b1782dc
Tristan Brown <tgbrown@stanford.edu>
2018-08-20 21:14:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  array = []
+  names.each {|name| array.push("Hello, my name is #{name}.")}
+  array
+end
+
+def assign_rooms(names)
+  new_array = []
+  names.each_with_index do |name, index|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+end
+  new_array
+end
+
+def printer(names)
+   batch_badge_creator(names).each do |badges|
+     puts badges
+   end
+   assign_rooms(names).each do |room_assignment|
+      puts room_assignment
+end
+end
