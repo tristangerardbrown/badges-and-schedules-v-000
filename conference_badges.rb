77930a83d6f51c7c65d4a680319eb89d7950b9e8
Tristan Brown <tgbrown@stanford.edu>
2018-08-20 18:53:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,12 @@
 def badge_maker(name)
 "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+  counter = 0
+  "Hello, my name is #{array[0]}."
+  county += 1
+when counter = 0
+end
+end 
